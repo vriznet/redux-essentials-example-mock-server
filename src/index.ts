446f21080cfce08cde79bc165bf4e25a0cd70c18
vriznet <vriz@vriz.net>
2023-11-17 16:35:57 +0900

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import execSchema from './execSchema';
 
+require('dotenv').config();
+
 const startServer = async () => {
+  const PORT = (process.env.PORT && +process.env.PORT) || 4000;
+
   const server = new ApolloServer({
     schema: execSchema,
   });
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port: PORT },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
