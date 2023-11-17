adf82a7bf1671275c31639337ac7c6df1f5f8204
vriznet <vriz@vriz.net>
2023-11-17 20:59:05 +0900

    Express4 및 그 미들웨어와 함께 동작하는 Apollo Server 4 기본 구현

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,43 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import execSchema from './execSchema';
+import express from 'express';
+import http from 'http';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import cors from 'cors';
+import { expressMiddleware } from '@apollo/server/express4';
+
+interface IContext {
+  token?: string;
+}
 
 require('dotenv').config();
 
 const startServer = async () => {
   const PORT = (process.env.PORT && +process.env.PORT) || 4000;
 
-  const server = new ApolloServer({
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer<IContext>({
     schema: execSchema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: PORT },
-  });
+  await server.start();
 
-  console.log(`🚀  Server ready at: ${url}`);
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 };
 
 startServer();
